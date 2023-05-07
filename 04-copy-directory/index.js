6c5b8a9579e0b85dcdcc6f9e57c37c1dd55e3b37
Liza <yelizaveta.ponomareva.2003@mail.ru>
2023-05-07 14:16:43 +0300

    feat: add copy-directory solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,44 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
+const fs=require('fs');
+
+const destinationPath = path.join(__dirname, 'files-copy');
+const sourcePath = path.join(__dirname, 'files');
+
+function copyDir(source, destination){
+  fs.readdir(source, (err, files) => {
+    fsPromises.mkdir(destination, { recursive: true })
+      .then(() => {
+        console.log('Директория успешно создана');
+      })
+      .catch((error) => {
+        console.error('Ошибка при создании директории:', error);
+      });
+    files.forEach((file) => {
+      const sourceFile = path.join(source, file);
+      const destinationFile = path.join(destination, file);
+      fs.copyFile(sourceFile, destinationFile, (err) => {
+        if (err) {
+          console.error('Ошибка при копировании файла:', err);
+        }
+      });
+    });
+  });
+  fs.readdir(destination, (err, files)=>{
+    files.forEach(file=>{
+      fs.access(path.join(__dirname, 'files', file), fs.constants.F_OK, (err) => {
+        if (err) {
+          fs.unlink(path.join(__dirname, 'files-copy', file), (err) => {
+            if (err) {
+              console.error('Ошибка при удалении файла:', err);
+            }
+          });
+        }
+      });
+    });
+  });
+}
+
+copyDir(sourcePath, destinationPath);
+
 
